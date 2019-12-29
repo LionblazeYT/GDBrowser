950adb884a9ccd29b47d8c491f061413e74388ef
Lionblaze <59327609+LionblazeYT@users.noreply.github.com>
2019-12-28 23:56:39 -0800

    minor compression
    improves performance with no structure change!

--- api/post/postComment.js
+++ api/post/postComment.js
@@ -3,48 +3,37 @@ const XOR = require('../../classes/XOR.js');
 const xor = new XOR();
 const crypto = require('crypto')
 function sha1(data) { return crypto.createHash("sha1").update(data, "binary").digest("hex"); }
-
 let rateLimit = {};
 let cooldown = 10000
-
 function getTime(time) {
   let seconds = Math.ceil(time / 1000);
   seconds = seconds % 60;
   return seconds}
-
 module.exports = async (app, req, res) => {
-
   if (!req.body.comment) return res.status(400).send("No comment provided!")
   if (!req.body.username) return res.status(400).send("No username provided!")
   if (!req.body.levelID) return res.status(400).send("No level ID provided!")
   if (!req.body.accountID) return res.status(400).send("No account ID provided!")
   if (!req.body.password) return res.status(400).send("No password provided!")
-
   if (req.body.comment.includes('\n')) return res.status(400).send("Comments cannot contain line breaks!")
-
   if (rateLimit[req.body.username]) return res.status(400).send(`Please wait ${getTime(rateLimit[req.body.username] + cooldown - Date.now())} seconds before posting another comment!`)
-  
   let params = {
     gameVersion: app.gameVersion,
     binaryVersion: app.binaryVersion,
     secret: app.secret,
     percent: 0
   }
-
   params.comment = new Buffer(req.body.comment + (req.body.color ? "☆" : "")).toString('base64').replace(/\//g, '_').replace(/\+/g, "-")
   params.gjp = xor.encrypt(req.body.password, 37526)
   params.levelID = req.body.levelID.toString()
   params.accountID = req.body.accountID.toString()
   params.userName = req.body.username
-
   let percent = parseInt(req.body.percent)
   if (percent && percent > 0 && percent <= 100) params.percent = percent.toString()
-
   let chk = params.userName + params.comment + params.levelID + params.percent + "0xPT6iUrtws0J"
   chk = sha1(chk)
   chk = xor.encrypt(chk, 29481)
   params.chk = chk
-
   request.post(app.endpoint + 'uploadGJComment21.php', {
     form: params
   }, function (err, resp, body) {
